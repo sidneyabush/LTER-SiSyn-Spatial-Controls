33aa4bbb64fb5d163cc84439bbc6016029ac98d3
Sidney Bush <bushsi@oregonstate.edu>
2025-08-13 22:07:52 -0700

    Update Step1_harmonization_for_cross_validation.R

--- Run_Test_Train_CrossValidation_Workflow/Step1_harmonization_for_cross_validation.R
+++ Run_Test_Train_CrossValidation_Workflow/Step1_harmonization_for_cross_validation.R
@@ -724,36 +724,26 @@ compute_split_metrics <- function(daily_df, siteyear_df, rbi_name, rcs_name) {
 }
 
 # metrics per partition
-unseen10_metrics <- compute_split_metrics(daily_kalman, unseen10_df, "RBI_unseen10", "slope_unseen10")
-older70_metrics  <- compute_split_metrics(daily_kalman, older70,      "RBI_older70",  "slope_older70")
-recent30_metrics <- compute_split_metrics(daily_kalman, recent30,     "RBI_recent30", "slope_recent30")
-
-# Create partition outputs
-unseen10_out <- unseen10_df %>%
-  dplyr::left_join(unseen10_metrics, by = "Stream_ID")
+older70_metrics  <- compute_split_metrics(daily_kalman, older70,      "RBI",  "recession_slope")
+recent30_metrics <- compute_split_metrics(daily_kalman, recent30,     "RBI", "recession_slope")
 
 older70_out <- older70 %>%
   dplyr::select(-any_of(c("RBI","recession_slope"))) %>%
-  dplyr::left_join(older70_metrics,  by = "Stream_ID")
+  dplyr::left_join(older70_metrics, by = "Stream_ID") %>%
+  dplyr::filter(recession_slope >= 0) %>%
+  dplyr::filter(complete.cases(.))
 
 recent30_out <- recent30 %>%
   dplyr::select(-any_of(c("RBI","recession_slope"))) %>%
-  dplyr::left_join(recent30_metrics, by = "Stream_ID")
-
-# sanity check: global values are gone from time-splits
-stopifnot(!any(c("RBI","recession_slope") %in% names(older70_out)))
-stopifnot(!any(c("RBI","recession_slope") %in% names(recent30_out)))
-
+  dplyr::left_join(recent30_metrics, by = "Stream_ID") %>%
+  dplyr::filter(recession_slope >= 0) %>%
+  dplyr::filter(complete.cases(.))
 
 # #############################################################################
 # 9. Combine and export all partitions
 # #############################################################################
-stopifnot(all(c("RBI_unseen10","slope_unseen10") %in% names(unseen10_out)))
-stopifnot(all(c("RBI_older70","slope_older70")   %in% names(older70_out)))
-stopifnot(all(c("RBI_recent30","slope_recent30") %in% names(recent30_out)))
-
-write.csv(unseen10_out, "AllDrivers_unseen10_not_split.csv", row.names = FALSE)
+write.csv(unseen10_df, "AllDrivers_unseen10_not_split.csv", row.names = FALSE)
 write.csv(older70_out,  "AllDrivers_older70_split.csv",      row.names = FALSE)
 write.csv(recent30_out, "AllDrivers_recent30_split.csv",     row.names = FALSE)
 
-
+#---- End of Script ----
